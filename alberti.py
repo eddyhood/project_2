2f2274d7e21cd5dfd9b3d2c6c1c920be04f55188
Eddy Hood <eddy@ignitespot.com>
2017-07-02 15:17:02 -0600

    refactored code

---
+++ alberti.py
@@ -0,0 +1,82 @@
+import logging
+import random
+from ciphers import Cipher
+
+logging.basicConfig(filename="cipherlogs.log", level=logging.INFO)
+
+class Alberti(Cipher):
+    """Encodes a message by using two different lists of letters / characters and matching the index value of the "movable" list to that of the fixed"""
+
+    encrpyted_message = []
+
+    fixed = ["a", "b", "c", "d", "e", "f", "g", "i", "l", "m", "n",
+                            "o", "p", "q", "r", "s", "t", "v", "x", "z",
+                            1, 2, 3, 4]
+
+    movable = ["d", "l", "g", "a", "z", "e", "n", "b", "o", "s",
+                       "f","c", "h", "t", "y", "q", "i", "x", "k", "v",
+                       "p", "&","m", "r"]
+
+    digraph = {"h":"f",
+               "j":"i",
+               "k":"q",
+               "u":"v",
+               "w":"x",
+               "y":"z"
+               }
+
+    pointer = "k"
+    shift_sequence = 5
+
+    @classmethod
+    def create_alberti(cls, *args, **kwargs):
+        #creates an instance of Alberti by getting message from user
+        message = input("Enter a message to encrypt / decrypt: ")
+        logging.info("User message created: {}".format(message))
+        return cls(message)
+
+    def spin(self):
+        pointer_index = self.movable.index(self.pointer)
+        letter_shift = random.choice(self.fixed[:20])
+        shift_index = self.fixed.index(letter_shift)
+        diff_index = pointer_index - shift_index
+        spin = self.movable[diff_index:] + self.movable[:diff_index]
+        add_key_letter = self.encrpyted_message.append(letter_shift.upper())
+
+        logging.info("pointer index: {0}, letter shift: {1}, shift index: {2}, diff index: {3}, spin: {4}".format(pointer_index, letter_shift, shift_index, diff_index, spin))
+        return spin
+
+    def add_digraphs(self):
+    #The cipher requires that certain letters be replaced with digraphs before the message can be encoded.
+        lower_case = self.message.lower()
+        message_with_digraphs = []
+
+        for letter in lower_case:
+            if letter == " ":
+                continue
+            elif letter in self.digraph:
+                message_with_digraphs.append(self.digraph[letter])
+                message_with_digraphs.append(self.digraph[letter])
+            else:
+                message_with_digraphs.append(letter)
+
+        logging.info("With digraph is {}".format(message_with_digraphs))
+        return message_with_digraphs
+
+    def encrypt(self):
+        add_digraphs = self.add_digraphs()
+        start_spin = self.spin()
+        count_spins = len(self.message) // self.shift_sequence
+
+        for spin in range(count_spins):
+            for letter in add_digraphs:
+                get_letter_index = self.fixed.index(letter)
+                get_coded_letter = self.movable[get_letter_index]
+                encrypt = self.encrpyted_message.append(get_coded_letter)
+
+        logging.info("Encrypted Message is: {} and total spins = {}".format(self.encrpyted_message, count_spins))
+
+
+
+
+
--- ciphers.py
+++ ciphers.py
@@ -1,4 +1,3 @@
-import os
 import logging
 
 logging.basicConfig(filename = "cipherlogs.log", level=logging.INFO)
@@ -8,90 +7,6 @@ class Cipher:
     """Parent class that all uniques ciphers extend from. Takes a single
     string argument from the user as the text to be encrypted or
     decrypted."""
-    def __init__(self, message, *args, **kwargs):
+    def __init__(self, message, touch_pad = None, *args, **kwargs):
         self.message = message
-
-
-class Alberti(Cipher):
-    """Encodes a message by getting a user generated key and using it to combine both the real_letters and encoded_letters lists for a cipher
-    """
-
-    message_formatted = []
-    message_with_digraph = []
-    encrpyted_message = []
-
-    real_letters = ["a", "b", "c", "d", "e", "f", "g", "i", "l", "m", "n",
-                            "o", "p", "q", "r", "s", "t", "v", "x", "z",
-                            1, 2, 3, 4]
-
-    encoded_letters = ["D", "L", "G", "A", "Z", "E", "N", "B", "O", "S",
-                       "F","C", "H", "T", "Y", "Q", "I", "X", "K", "V",
-                       "P", "&","M", "R"]
-
-    digraph = {"H":"FF",
-               "J":"II",
-               "K":"QQ",
-               "U":"VV",
-               "W":"XX",
-               "Y":"ZZ"
-               }
-
-    secret_marker = "K"
-    secret_marker_index = encoded_letters.index(secret_marker)
-
-    def __init__(self, message, letter_key, *args, **kwargs):
-        super().__init__(message, *args, **kwargs)
-        self.letter_key = letter_key
-
-        #Check for letter in possible options
-        if letter_key in ["H", "J", "K", "U", "W", "Y"]:
-            logging.warning("User entered something other than a letter.")
-            raise ValueError("That letter will not work.  Remember that H, J, K, U, W, and Y are not options for a key")
-
-        logging.info("User message: {}, user key: {}".format(self.message, letter_key))
-
-    @classmethod
-    def create_alberti(cls, *args, **kwargs):
-        #creates an instance of Alberti by getting message & key from user
-        message = input("Enter a message to encrypt / decrypt: ")
-        letter_key = input("Enter a single letter as a key.  Note that H, J, K, U, W, and Y are not options for a key: ").upper()
-        return cls(message, letter_key)
-
-    def format_alberti(self):
-        #Before you can encrypt, each word should start with a caps to make the cipher stronger.
-        cap_word = self.message.title()
-        for letter in cap_word:
-            if letter == " ":
-                continue
-            else:
-                self.message_formatted.append(letter)
-        logging.info("by letter is {}".format(self.message_formatted))
-
-    def add_digraphs(self):
-        #The cipher requires that certain letters be replaced with digraphs before the message can be encoded.
-
-        for letter in self.message_formatted:
-            if letter.upper() in self.digraph:
-                self.message_with_digraph.append(self.digraph[letter.upper()])
-            else:
-                self.message_with_digraph.append(letter)
-
-        logging.info("With digraph is {}".format(self.message_with_digraph))
-
-    def encrypt(self):
-
-        #complete encryption
-        key_index = self.real_letters.index(self.letter_key.lower())
-        index_diff = self.secret_marker_index - key_index
-        logging.info("Starting point is: {}".format(key_index))
-        logging.info("Spin the dial by: {}".format(index_diff))
-
-        spin = self.encoded_letters[index_diff:] + self.encoded_letters[:index_diff]
-
-        logging.info("spin is {}".format(spin))
-        return spin
-
-
-
-    def decrypt(self):
-        pass
+        self.touch_pad = touch_pad
